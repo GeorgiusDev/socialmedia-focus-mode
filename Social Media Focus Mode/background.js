6143983e241b4e7f7adfb480fba2b5d055d69f23
Georgius <juraj.pazin@gmail.com>
2025-12-17 12:04:40 +0100

    Retrieve and set storage values on installation

--- Social Media Focus Mode/background.js
+++ Social Media Focus Mode/background.js
@@ -1,3 +1,8 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.local.set({ enabled: true });
+    chrome.storage.local.get(
+        { ytEnabled: true, igEnabled: true },
+        data => {
+            chrome.storage.local.set(data);
+        }
+    );
 });
